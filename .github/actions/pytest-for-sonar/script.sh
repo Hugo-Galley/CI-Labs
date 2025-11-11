d167908042a8e17ede91d82d6a4fe8922bf1e60b
Freaks <galleyhugo@icloud.com>
2025-11-11 19:04:29 +0100

    on remet au propre

--- .github/actions/pytest-for-sonar/script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-MODULE="$1"
-pytest --cov=$MODULE --cov-report=xml
-
-if grep -q "$GITHUB_WORKSPACE" coverage.xml; then
-            sed -i "s|$GITHUB_WORKSPACE/||g" coverage.xml || true
-            head -n 40 coverage.xml || true
-fi
