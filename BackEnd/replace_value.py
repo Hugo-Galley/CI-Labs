c8f7a7c1c58ee013bace1414542a4f498d05494e
Freaks <galleyhugo@icloud.com>
2025-11-15 23:28:52 +0100

    encore

--- BackEnd/replace_value.py
+++ BackEnd/replace_value.py
@@ -1,15 +1,20 @@
 import json
 import os
 
-with open("BackEnd/variables.json", "r") as f:
+base_dir = os.path.dirname(__file__)
+vars_path = os.path.join(base_dir, "variables.json")
+
+with open(vars_path, "r") as f:
     data = json.load(f)
 
-data["Log"]["FileDestination"] = os.environ["FILE_DESTINATION"]
-data["Bdd"]["Host"] = os.environ["BDD_HOST"]
-data["Bdd"]["Database"] = os.environ["BDD_DATABASE"]
-data["Bdd"]["User"] = os.environ["BDD_USER"]
-data["Bdd"]["Password"] = os.environ["BDD_PASSWORD"]
-data["Bdd"]["Port"] = os.environ["BDD_PORT"]
+data["Log"]["FileDestination"] = os.environ.get(
+    "FILE_DESTINATION", data["Log"].get("FileDestination")
+)
+data["Bdd"]["Host"] = os.environ.get("BDD_HOST", data["Bdd"].get("Host"))
+data["Bdd"]["Database"] = os.environ.get("BDD_DATABASE", data["Bdd"].get("Database"))
+data["Bdd"]["User"] = os.environ.get("BDD_USER", data["Bdd"].get("User"))
+data["Bdd"]["Password"] = os.environ.get("BDD_PASSWORD", data["Bdd"].get("Password"))
+data["Bdd"]["Port"] = os.environ.get("BDD_PORT", data["Bdd"].get("Port"))
 
-with open("variables.json", "w") as f:
+with open(vars_path, "w") as f:
     json.dump(data, f, indent=4)
